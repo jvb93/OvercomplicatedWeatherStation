8bda9dc7d24e29c4bfc6f809482a8301a509959d
jvb93 <1976577+jvb93@users.noreply.github.com>
2020-08-29 12:01:53 -0700

    enhanced frontend footer

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="container is-fullhd">
-    <b-switch v-model="useImperial" size="is-small" class="has-text-light">imperial</b-switch>
     <div class="tile is-ancestor mt-6">
       <div class="tile is-vertical is-10">
         <div class="tile">
@@ -15,6 +14,9 @@
         <div class="tile is-parent is-vertical">
           <weather-chart />
         </div>
+        <div class="tile is-parent is-vertical">
+          <app-footer />
+        </div>
       </div>
     </div>
     <span id="background" :class="backgroundClass"></span>
@@ -26,9 +28,16 @@ import CurrentConditions from "@/components/weather/CurrentConditions.vue";
 import WeatherChart from "@/components/weather/WeatherChart.vue";
 import Battery from "@/components/device/Battery.vue";
 import Barometer from "@/components/weather/Barometer.vue";
+import AppFooter from "@/components/AppFooter.vue";
 export default {
   name: "App",
-  components: { CurrentConditions, Battery, WeatherChart, Barometer },
+  components: {
+    CurrentConditions,
+    Battery,
+    WeatherChart,
+    Barometer,
+    AppFooter,
+  },
   computed: {
     backgroundClass() {
       let now = new Date();
@@ -42,22 +51,6 @@ export default {
         return "dusk";
       }
       return "night";
-    },
-    useImperial: {
-      // getter
-      get: function () {
-        return localStorage["useImperial"];
-      },
-      // setter
-      set: function (newValue) {
-        if (newValue) {
-          localStorage["useImperial"] = true;
-        } else {
-          localStorage["useImperial"] = false;
-        }
-
-        location.reload();
-      },
     },
   },
 };
@@ -126,5 +119,10 @@ export default {
   100% {
     background-position: 0% 50%;
   }
+}
+
+.fixed-footer {
+  position: fixed;
+  bottom: 0px;
 }
 </style>
---
+++ frontend/src/components/AppFooter.vue
@@ -0,0 +1,46 @@
+<template>
+  <article class="tile is-parent is-vertical">
+    <nav class="level">
+      <div class="level-left">
+        <div class="level-item">
+          <a
+            class="button is-light is-outlined is-small"
+            href="https://github.com/jvb93/OvercomplicatedWeatherStation"
+            target="_blank"
+            >Made by @jvb93
+          </a>
+        </div>
+      </div>
+      <div class="level-right">
+        <p class="level-item">
+          <b-switch v-model="useImperial" size="is-small" class="has-text-light"
+            >imperial units</b-switch
+          >
+        </p>
+      </div>
+    </nav>
+  </article>
+</template>
+
+<script>
+export default {
+  computed: {
+    useImperial: {
+      // getter
+      get: function() {
+        return localStorage["useImperial"];
+      },
+      // setter
+      set: function(newValue) {
+        if (newValue) {
+          localStorage["useImperial"] = true;
+        } else {
+          localStorage["useImperial"] = false;
+        }
+
+        location.reload();
+      },
+    },
+  },
+};
+</script>
