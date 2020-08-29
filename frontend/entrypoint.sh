1fcc8f746424777d3c49ad5763737f2c53971925
jvb93 <1976577+jvb93@users.noreply.github.com>
2020-08-29 14:19:46 -0700

    rearrange shell script

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+echo "Starting Nginx"
+nginx -g 'daemon off;'
+
+echo "Starting substitution"
 for file in /usr/share/nginx/html/js/app.*.js;
 do
   echo $file
@@ -7,5 +12,3 @@ do
   fi
   envsubst '$VUE_APP_BASE_URL' < $file.tmpl.js > $file
 done
-echo "Starting Nginx"
-nginx -g 'daemon off;'
